9cfbbaf53394681be83c02b189f5d1d133c8312f
Olivia Smith <116675192+omsmith161@users.noreply.github.com>
2024-05-06 16:07:02 -0400

    Update code-used.sh
    Finished editing the .sh file to include all of our commands from genome assembly and analysis.

--- code-used.sh
+++ code-used.sh
@@ -54,7 +54,71 @@ prokka spades_assembly/contigs.fasta --outdir prokka_output --cpus 24 --minconti
 grep -o "product=.*" ./prokka_output/PROKKA_*.gff | sed 's/product=//g' | sort | uniq -c | sort -nr > protein_abundances.txt
 mv protein_abundances.txt prokka_output
 
+# we extracted the 16s rRNA information from the PROKKA .ffn file to use in our BLAST analysis
+grep 16S prokka_output/*.ffn
+extract_sequences "16S ribosomal RNA" prokka_output/PROKKA_*.ffn > organism_identification/16S_sequence.fasta
 
+# we used BLAST on the NCBI website to look for species that match our 16s rRNA 
+# https://blast.ncbi.nlm.nih.gov/Blast.cgi
+# to do this, we copied and pasted the 16s rRNA sequence into BLAST and waited for the program to match with other species in the database
+nano 16S_sequence.fasta
 
+# we did read mapping using samtools to map the forward and reverse reads onto our completed reference genome
+mkdir read_mapping
+cp ../spades_assembly/contigs.fasta .
+bwa index contigs.fasta
+bwa mem -t 24 contigs.fasta ../reads_trimmed/trimmed-reads/36_S3_L001_R1_001.fastq.gz ../reads_trimmed/trimmed-reads/36_S3_L001_R2_001.fastq.gz > raw_mapped.sam
+samtools view -@ 24 -Sb  raw_mapped.sam  | samtools sort -@ 24 - sorted_mapped
+samtools flagstat sorted_mapped.bam
+samtools index sorted_mapped.bam
+bedtools genomecov -ibam sorted_mapped.bam > coverage.out
+gen_input_table.py  --isbedfiles contigs.fasta coverage.out >  coverage_table.tsv
 
+# ran BLAST again using command line because we realized we were missing a file to use in the non-target contig removal step
+mkdir blast
+blob_blast.sh ../spades_assembly/contigs.fasta 
+
+# we completed non-target contig removal to prepare our data for visualization
+# visualization was completed using blobtools and our ooutput files were brought to our local devices
+mkdir contig_removal
+blobtools create -i ../spades_assembly/contigs.fasta -b ../read_mapping/sorted_mapped.bam -t ../blast/contigs.fasta.vs.nt.cul5.1e5.megablast.out -o blob_out
+blobtools view -i blob_out.blobDB.json -r all -o blob_taxonomy
+grep -v '##' blob_taxonomy.blob_out.blobDB.table.txt
+blobtools plot -i blob_out.blobDB.json -r genus
+# (In local terminal)
+cd contig_removal
+lcd Desktop
+get *.png
+
+# we filtered our outputs by both length and coverage
+mkdir filtered_assembly
+cp contig_removal/blob_taxonomy.blob_out.blobDB.table.txt .
+mv blob_taxonomy.blob_out.blobDB.table.txt ./filtered_assembly/
+grep -v '#' blob_taxonomy.blob_out.blobDB.table.txt | awk -F'\t' '$2 > 500' | wc -w
+grep -v '#' blob_taxonomy.blob_out.blobDB.table.txt | awk -F'\t' '$2 < 500' | wc -w
+grep -v '#' blob_taxonomy.blob_out.blobDB.table.txt | awk -F'\t' '$2 > 500' | awk -F'\t' '$5 < 5' | wc -w
+grep -v '#' blob_taxonomy.blob_out.blobDB.table.txt | awk -F'\t' '$2 > 500' | awk -F'\t' '$5 > 5' | wc -w
+
+# we constructed a list of contigs to keep from filtering and put them into a file called "list_of_contigs_to_keep_len500_cov20.txt"
+grep -v '##' blob_taxonomy.blob_out.blobDB.table.txt | awk -F'\t' '$2 > 500' | awk -F'\t' '$5 > 20' | less
+grep -v '##' blob_taxonomy.blob_out.blobDB.table.txt | awk -F'\t' '$2 > 500' | awk -F'\t' '$5 > 20' | awk -F'\t' '{print $1}' > list_of_contigs_to_keep_len500_cov20.txt
+
+# we filtered the genome assembly by the list of contigs that we wanted to keep from filtering
+# called our final, filtered assembly "Microbacterium.fasta"
+filter_contigs_by_list.py spades_assembly/contigs.fasta filtered_assembly/list_of_contigs_to_keep_len500_cov20.txt Microbacterium.fasta
+
+# we looked at the coverage across our contigs from the blobtools table that we created
+grep -f filtered_assembly/list_of_contigs_to_keep_len500_cov20.txt filtered_assembly/blob_taxonomy.blob_out.blobDB.table.txt | awk '{w = w + $2; e = e + $5 * $2;} END {print e/w}'
+
+# we ran BLAST again on our final alignment against UniVec to make sure there was no contamination
+wget "https://ftp.ncbi.nlm.nih.gov/pub/UniVec/UniVec"
+blastn -reward 1 -penalty -5 -gapopen 3 -gapextend 3 -dust yes -soft_masking true -evalue 700 -searchsp 1750000000000 -query ../Microbacterium.fasta -subject UniVec  -outfmt 6 -out genome_vs_univec.6
+
+# ran QUAST and BUSCO again to see how our original assembly compared to our filtered assembly
+quast.py Microbacterium.fasta -o quast_again
+conda activate busco 
+busco -i Microbacterium.fasta -m genome -o busco_again -l bacteria
+
+# indexed the Microbacterium.fasta file using samtools to make it suitable for IGV Web Viewer visualization
+samtools faidx ../Microbacterium.fasta
 
